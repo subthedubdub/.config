b46f94a1748dada90c571251c53444ab07a330c3
Michael Kovarik <Michael Kovarik>
2023-09-24 15:08:17 -0400

    ass emacs config

---
+++ emacs/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(julia-snail eglot-jl vterm which-key treemacs-evil magit julia-mode general flycheck evil-collection eglot company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
